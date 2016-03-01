2983126b5ae53ea1466a6ce20fb62727f600feb0
S-C-M <castanedasergio321@gmail.com>
2016-03-01 23:17:10 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  post'/' do
+    @user = {
+      name: "#{params["fname"]} #{params["lname"]}",
+      profile_pic_url: "#{params["picurl"]}"
+      }
+    erb :results
+  end
+  
+end
